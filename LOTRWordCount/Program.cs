5862a859f323cbd0247396569e5ea194d89ce2de
jbyfordrew <jeremybyfordrew@gmail.com>
2019-04-18 06:57:07 +0100

    initial app run UI

--- LOTRWordCount/Program.cs
+++ LOTRWordCount/Program.cs
@@ -6,7 +6,31 @@ namespace LOTRWordCount
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StartApp();
+            OutputResult();
+            ExitApp();
+        }
+
+        private static void OutputResult()
+        {
+            Console.WriteLine("output"); //TODO            
+        }
+
+        private static void StartApp()
+        {
+            Console.WriteLine("Word occurrences count in text document.");
+            Console.WriteLine();
+            Console.Write("Press any key to start.");
+            Console.ReadKey();
+            Console.WriteLine("\rProcessing ...         ");
+            Console.WriteLine();
+        }
+
+        private static void ExitApp()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }

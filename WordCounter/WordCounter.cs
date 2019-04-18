728901b966b728e7ffe61dfb7e366da73ae8a5eb
jbyfordrew <jeremybyfordrew@gmail.com>
2019-04-18 07:26:12 +0100

    back to failing again to implement

--- WordCounter/WordCounter.cs
+++ WordCounter/WordCounter.cs
@@ -7,19 +7,7 @@ namespace WordCounter
     {
         public IList<KeyValuePair<string,int>> CountWords(string source)
         {
-            return new List<KeyValuePair<string, int>>()
-            {
-                new KeyValuePair<string,int>( "a", 1),
-                new KeyValuePair<string,int>( "b", 2),
-                new KeyValuePair<string,int>( "c", 3),
-                new KeyValuePair<string,int>( "d", 4),
-                new KeyValuePair<string,int>( "e", 5),
-                new KeyValuePair<string,int>( "f", 6),
-                new KeyValuePair<string,int>( "g", 7),
-                new KeyValuePair<string,int>( "h", 8),
-                new KeyValuePair<string,int>( "i", 9),
-                new KeyValuePair<string,int>( "j", 10),
-            };
+            return new List<KeyValuePair<string, int>>();
         }
     }
 }
